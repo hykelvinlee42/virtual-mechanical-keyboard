198a0df5d08f81e897b436646c91e29551a0672f
HY. Kelvin Lee <hykelvinlee42@gmail.com>
2022-12-22 14:32:30 -0500

    implemented switch selection menu

--- keyboard.py
+++ keyboard.py
@@ -1,6 +1,6 @@
+from InquirerPy import inquirer, prompt
 from pydub.playback import _play_with_simpleaudio as play
 from pynput import keyboard
-
 from switch import Switch, Switch_Type
 
 sound = None
@@ -22,13 +22,27 @@ def on_release(key):
     if key == keyboard.Key.esc:  # stop listener
         return False
 
+def get_switch_type():
+    switch_type = Switch_Type.GX_BLUE
+    input_options = prompt([
+        {
+            "type": "list",
+            "name": "type",
+            "message": "Pick your favourite switch",
+            "choices": [switch.name for switch in list(Switch_Type)]
+        }
+    ])
+
+    switch_type = Switch_Type[input_options.get("type")]
+    return switch_type
+
 def main():
     global sound
-    switch = Switch(switch_type=Switch_Type.GX_BLUE)  # I'm about to end my girlfriend ears
+    switch_type = get_switch_type()
+    switch = Switch(switch_type=switch_type)  # I'm about to end my girlfriend ears
     sound = switch.getsound()
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
-
 
 
 if __name__ == "__main__":
